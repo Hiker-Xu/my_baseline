0012ef5afb056c73eedf4c436d69abd0dea8d81a
Hiker-Xu <37582195+Hiker-Xu@users.noreply.github.com>
2020-03-29 10:45:28 +0800

    Add files via upload

---
+++ src/data_process.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+import numpy as np
+
+def unNormalizeData(normalized_data, data_mean, data_std, dimensions_to_use):
+    T = normalized_data.shape[0]  # batch size
+    D = data_mean.shape[0] 
+
+    orig_data = normalized_data
+
+    #orig_data = np.zeros((T,D), dtype=np.float32)
+
+    #orig_data[:, ] = 
+
+    # Multiply times stdev and add the mean
+    stdMat = data_std.reshape((1, D))
+    stdMat = np.repeat(stdMat, T, axis=0)
+    meanMat = data_mean.reshape((1, D))
+    meanMat = np.repeat(meanMat, T, axis=0)
+    orig_data = np.multiply(orig_data, stdMat) + meanMat
+    return orig_data
+  
---
+++ src/datasets/surreal24.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function, absolute_import
+
+import os
+import torch
+from torch.utils.data import Dataset
+
+
+class SURREAL24(Dataset):
+    def __init__(self, data_path, is_train=True):
+        """
+        :param data_path: path to dataset
+        :param is_train: load train/test dataset
+        """
+
+        self.data_path = data_path
+        self.is_train = is_train
+
+        self.train_inp, self.train_out, self.test_inp, self.test_out = [], [], [], []
+        self.train_meta, self.test_meta = [], []
+
+        # loading data
+        train_2d_file = 'train_2d.npy'
+        test_2d_file = 'test_2d.npy'
+
+        if self.is_train:
+            # load train data
+            self.train_3d = np.load(os.path.join(data_path, 'train_3d.npy'))
+            self.train_2d = np.load(os.path.join(data_path, train_2d_file))
+            num_f = self.train_2d.shape[0]
+            assert self.train_2d.shape[0] == self.train_3d.shape[0], '(training) 3d & 2d shape not matched' 
+            self.train_inp = self.train_2d.reshape(num_f, 48)
+            self.train_out = self.train_3d.reshape(num_f, 72)
+
+        else:
+            # load test data
+            self.test_3d = np.load(os.path.join(data_path, 'test_3d.npy'))
+            self.test_2d = np.load(os.path.join(data_path, test_2d_file))
+            num_f = self.test_2d.shape[0]
+            assert self.test_2d.shape[0] == self.test_3d.shape[0], '(testing) 3d & 2d shape not matched' 
+            self.test_inp = self.test_2d.reshape(num_f, 48)
+            self.test_out = self.test_3d.reshape(num_f, 72)
+        
+    def __getitem__(self, index):
+        if self.is_train:
+            inputs = torch.from_numpy(self.train_inp[index]).float()
+            outputs = torch.from_numpy(self.train_out[index]).float()
+        else:
+            inputs = torch.from_numpy(self.test_inp[index]).float()
+            outputs = torch.from_numpy(self.test_out[index]).float()
+
+        return inputs, outputs
+
+    def __len__(self):
+        if self.is_train:
+            return len(self.train_inp)
+        else:
+            return len(self.test_inp)  
